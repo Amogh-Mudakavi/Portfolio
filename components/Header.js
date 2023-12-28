32452acfdb67bca8c0459ecc3a0c7c7fed46451f
Amogh-Mudakavi <151946989+Amogh-Mudakavi@users.noreply.github.com>
2023-12-28 12:48:24 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
     <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
       {/* logo */}
       <Link href = {'/'}>
-      <Image src = {'/logog.png'} 
+      <Image src = {'./logog.png'} 
       width = {220}
        height = {48} 
        alt = ''

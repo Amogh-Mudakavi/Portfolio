843c900afb17759add62068244c2b0c9c3b869d6
Amogh-Mudakavi <mudakavirekha4@gmail.com>
2023-12-28 14:15:35 +0530

    ./

--- components/Avatar.js
+++ components/Avatar.js
@@ -8,7 +8,7 @@ const Avatar = () => {
   return (
   <div className="hidden xl:flex xl:max-w-none">
     <Image 
-    src={`/coder.png?${new Date().getTime()}`}
+    src={`./coder.png?${new Date().getTime()}`}
     width={737} 
     height={678} 
     alt="Coder Image"

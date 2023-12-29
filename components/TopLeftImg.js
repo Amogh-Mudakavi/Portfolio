361cef2a0082e29a3c80409682e309ffa8c1d9d9
Amogh-Mudakavi <151946989+Amogh-Mudakavi@users.noreply.github.com>
2023-12-29 20:38:23 +0530

    Update TopLeftImg.js

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
-
+import u from "../public/top-left-img.png"
 
 const TopLeftImg = () => {
   return (
   <div className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opacity-50">
-    <Image src = './top-left-img.png' width={400} height={400} alt = ''/>
+    <Image src ={u.src} width={400} height={400} alt = ''/>
   </div>
   );
 };

627afa76abb0f1c5655b780b405dc2fe40f47684
Amogh-Mudakavi <151946989+Amogh-Mudakavi@users.noreply.github.com>
2023-12-29 20:26:49 +0530

    Update Avatar.js

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,18 +1,23 @@
 //next image
 
+//next image
+
 import Image from "next/image";
+import f from '../public/coder.png'
+
 
 
 const Avatar = () => {
   return (
   <div className="hidden xl:flex xl:max-w-none">
     <Image 
-    src = './coder.png'
+    src={f.src}
     width={737} 
     height={678} 
-    alt="" 
+    alt=""
     className="translate-z-0 w-full h-full"
-      />
+/>
+
   </div>
   );
 };

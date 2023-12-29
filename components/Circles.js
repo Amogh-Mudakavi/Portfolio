fe12c4773364c62ac85373479ec119230b46ccc2
Amogh-Mudakavi <151946989+Amogh-Mudakavi@users.noreply.github.com>
2023-12-29 20:36:42 +0530

    Update Circles.js

--- components/Circles.js
+++ components/Circles.js
@@ -1,8 +1,8 @@
 import Image from "next/image"
-
+import qq from "../public/circles.png"
 const Circles = () => {
   return <div className="w-[200px] xl:w-[300px] absolute -right-16 -bottom-2 mix-blend-color-dodge animate-pulse duration-75 z-10">
-    <Image src = {'./circles.png'}
+    <Image src = {qq.src}
      width={260} height={200}
      className="w-full h-full" 
     alt =''

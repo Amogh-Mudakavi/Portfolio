4e5197fe9f60a53067db43a99ce3e9641d875c09
Amogh-Mudakavi <151946989+Amogh-Mudakavi@users.noreply.github.com>
2023-12-30 12:22:05 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
   return(<header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
 
     <div className="container mx-auto">
-    <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
+     <div className="py-4 gap-y-2 flex flex-col lg:flex-row justify-between items-center md:gap-y-6  md:py-8">
       {/* logo */}
       <Link href = {'/'}>
       <Image src = {t.src} 

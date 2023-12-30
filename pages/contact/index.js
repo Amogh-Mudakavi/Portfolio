d030bc7813173b96d7986a3f19c06125273064c0
Amogh-Mudakavi <151946989+Amogh-Mudakavi@users.noreply.github.com>
2023-12-30 12:19:53 +0530

    Update index.js

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -15,7 +15,7 @@ export const ContactUs = () => {
               <input type='email' name='user_email' placeholder='Email' className='input' />
             </div>
             <input type='text' name='subject' placeholder='Subject' className='my-5 input' />
-            <textarea name='message' placeholder='Message' className='textarea'></textarea>
+            <textarea name='message' placeholder='Message' className='textarea  h-1/4 md:h-44'></textarea>
             <button type='submit' className='btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group'>
               <span className='group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500'>Let&apos;s talk</span>
               <BsArrowRight className='-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]'/>

28005a41707ec376ec1d4653b2a0978c6d2f655e
Amogh-Mudakavi <151946989+Amogh-Mudakavi@users.noreply.github.com>
2024-01-01 20:55:33 +0530

    Update index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -128,7 +128,7 @@ const About = () => {
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="h2"
+            className="h2 md:text-5xl"
           >
             Mastering cutting edge{" "}
             <span className="text-accent">technology </span>tools
@@ -189,7 +189,7 @@ const About = () => {
             </div>
           </motion.div>
         </div>
-        <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
+        <div className=" flex flex-col w-full xl:max-w-[48%] h-[20rem] z-10">
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
             {aboutData.map((item, itemIndex) => {
               return (
